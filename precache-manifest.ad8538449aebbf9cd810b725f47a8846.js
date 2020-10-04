55e722b503465254d333230c5a6bb7f803094aee
Văn Kiên <kienquat2005@gmail.com>
2020-10-04 22:08:24 +0700

    Updates

---
+++ precache-manifest.ad8538449aebbf9cd810b725f47a8846.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f374aa3b0fe503fa4cb601b1acff607f",
+    "url": "/CV/index.html"
+  },
+  {
+    "revision": "cdee110a86555afc22b7",
+    "url": "/CV/static/css/2.9a9a1f6f.chunk.css"
+  },
+  {
+    "revision": "87bb96530a75bea07c66",
+    "url": "/CV/static/css/main.142fbfb5.chunk.css"
+  },
+  {
+    "revision": "cdee110a86555afc22b7",
+    "url": "/CV/static/js/2.319e5d15.chunk.js"
+  },
+  {
+    "revision": "f25112c0081db71b0e5dd2ce64e909e6",
+    "url": "/CV/static/js/2.319e5d15.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "87bb96530a75bea07c66",
+    "url": "/CV/static/js/main.42ecebf9.chunk.js"
+  },
+  {
+    "revision": "e4d273214469930d29bd",
+    "url": "/CV/static/js/runtime-main.4d0d12ac.js"
+  },
+  {
+    "revision": "3f529d0c7a60251f0331223e75fb8035",
+    "url": "/CV/static/media/profile.3f529d0c.png"
+  }
+]);
